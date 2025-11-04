529b943467b54f3b112370090f278f629e583ca8
Lochinbek Dehkonov <103257271+lochinbek9@users.noreply.github.com>
2025-11-04 12:36:43 +0500

    Update index.js

--- backEnd(server)/index.js
+++ backEnd(server)/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const movieRoute = require('./routes/movies');
@@ -16,6 +17,7 @@ mongoose
 		console.error(err);
 	});
 
+app.use(cors()); 
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
